3f2447904d29724adbb0b655a5f3435c258fd3d8
Dicky jr <lmayhenderson@yahoo.com>
2024-03-13 19:39:39 -0700

    added input.js for setUpInput function

--- play.js
+++ play.js
@@ -1,24 +1,9 @@
 const connect = require('./client');
+const setupInput = require('./input')
 
 // establishes a connection with the game server
 
 console.log("Connecting ...");
 connect();
-// setup interface to handle user input from stdin
-const handleUserInput = function(data) {
-	if (data === "\u0003") {
-		process.exit();
-	}
-};
-
-const setupInput = function() {
-	const stdin = process.stdin;
-	stdin.setRawMode(true);
-	stdin.setEncoding("utf8");
-	stdin.resume();
-	stdin.on("data", handleUserInput);
-	return stdin;
-};
-
 setupInput();
 
